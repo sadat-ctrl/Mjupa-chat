23bf78387104037e2af93e0bec1e5b36983f5704
sadat-ctrl <sadatimjupa@gmail.com>
2026-04-27 16:20:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,21 @@
+const CACHE = 'mjupa-v1';
+const FILES = [
+  '/Mjupa-chat/',
+  '/Mjupa-chat/index.html',
+  '/Mjupa-chat/manifest.json'
+];
+
 self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    fetch(e.request).catch(() => 
+      caches.match(e.request)
+    )
+  );
 });
